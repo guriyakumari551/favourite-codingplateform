c625dc67f6551189e03bb3065ea287f24d0b8d2f
Guriya_kumari <81032463+guriyakumari551@users.noreply.github.com>
2021-06-22 01:35:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+
+<div>
+
+<b>CODING  PLATEFORM</b>
+<p> List of 5 my favourite coding website</p>
+<ol>
+   <li> leetcode</li>
+   <li>Hackerearth</li>
+   <li>Geeksforgeeks</li>
+   <li>codeforces</li>
+   <li>Interviewbit</li>
+
+</ol>
+
+
+
+</div>,
+
+
+
+
+
+
+
+document.getElementById("root"));
